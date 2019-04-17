6d22f596d1288465aaed34c60d0f298ba06f042e
RodrigoM <rmmourao@gmail.com>
2019-04-17 19:09:17 -0300

    Aatualizados index.html e principal.js

--- Curso Javascript - Alura/js/principal.js
+++ Curso Javascript - Alura/js/principal.js
@@ -1,5 +1,14 @@
-console.log("Carregado de um arquivo externo.");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura*2);
+
+console.log(imc);
