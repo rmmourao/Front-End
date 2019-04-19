ec5390c4cafc2ff72820064ce4cac5d909ab5f39
RodrigoM <rmmourao@gmail.com>
2019-04-19 11:57:43 -0300

    Criado cadastro de usuários na web page

--- Curso Javascript - Alura/js/principal.js
+++ Curso Javascript - Alura/js/principal.js
@@ -3,10 +3,7 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-console.log(pacientes.length);
-
 for(var i = 0; i < pacientes.length; i++){
-    console.log(pacientes[i]);
 
     var paciente = pacientes[i];
 
@@ -41,7 +38,46 @@ for(var i = 0; i < pacientes.length; i++){
     }
 }
 
+/*titulo.addEventListener("click", mostraMensagem);
 
+function mostraMensagem(){
+    console.log("Olá eu fui clicado");
+}*/
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    paciente.appendChild(nomeTd);
+    paciente.appendChild(pesoTd);
+    paciente.appendChild(alturaTd);
+    paciente.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+});
 
 //console.log(pacientes);
