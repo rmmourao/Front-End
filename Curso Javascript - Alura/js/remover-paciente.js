92d3b88a39fc3c4871d56c53581032bee2aaac2a
RodrigoM <rmmourao@gmail.com>
2019-04-19 20:48:47 -0300

    Atualizações

--- Curso Javascript - Alura/js/remover-paciente.js
+++ Curso Javascript - Alura/js/remover-paciente.js
@@ -1,10 +1,9 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-console.log(pacientes);
+tabela.addEventListener("dblclick", function(event){	
+	event.target.parentNode.classList.add("fadeOut");
 
-pacientes.forEach(function(paciente){
-	paciente.addEventListener("dblclick", function(){
-		console.log("Duplo click");
-		this.remove();
-	});
+	setTimeout(function(){
+		event.target.parentNode.remove();
+	}, 500);
 });
