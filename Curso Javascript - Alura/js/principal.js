8c608160156787b6f4d28bb8d9e9dfd0c13e9b48
RodrigoM <rmmourao@gmail.com>
2019-04-19 00:06:55 -0300

    Incluido arquivo notes.txt e atualizado principal.js

--- Curso Javascript - Alura/js/principal.js
+++ Curso Javascript - Alura/js/principal.js
@@ -9,6 +9,27 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura*2);
+var tdImc = paciente.querySelector(".info-imc");
 
-console.log(imc);
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido.");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido.";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida.");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida.";
+}
+
+if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+}
+
+
+//console.log(tdImc);
