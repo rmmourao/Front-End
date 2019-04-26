c502015685502d08692072e5b5aa257ad46e08a1
RodrigoM <rmmourao@gmail.com>
2019-04-26 16:06:27 -0300

    Atualizações no código

--- Curso Vue.JS - Alura/alurapic/src/components/shared/painel/Painel.vue
+++ Curso Vue.JS - Alura/alurapic/src/components/shared/painel/Painel.vue
@@ -1,24 +1,40 @@
 <template>
+
     <div class="painel">
-        <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-        <div class="painel-conteudo">
-        </div>
+        <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+        <transition name="painel-fade">
+          <div class="painel-conteudo" v-show="visivel">
+            <slot></slot>
+          </div>
+        </transition>
     </div>
+
 </template>
 
 <script>
-export default {
-}
 
+export default {
+
+  props: ['titulo'],
+
+  data() {
+
+    return {
+      visivel: true
+    }
+
+  }
+}
 </script>
 
-<style>
+<style scoped>
+
   .painel {
     padding: 0 auto;
-    border: solid 2 px grey;
+    border: solid 2px grey;
     display: inline-block;
     margin: 5px;
-    box-shadow: 5px 5px 10 px grey;
+    box-shadow: 5px 5px 10px grey;
     width: 200px;
     height: 100%;
     vertical-align: top;
@@ -32,6 +48,18 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0;
+  }
+
+  .painel-fade-enter-active, .painel-fade-leave-active {
+    transition: opacity .4s;
   }
 
 </style>
