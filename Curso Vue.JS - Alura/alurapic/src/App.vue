84b2a06e740eeb12b330f233fc8bc220162e25a5
RodrigoM <rmmourao@gmail.com>
2019-04-26 12:32:28 -0300

    Adicionado arquivos ao repositorio

--- Curso Vue.JS - Alura/alurapic/src/App.vue
+++ Curso Vue.JS - Alura/alurapic/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
       </li>
     </ul>
 
@@ -33,4 +37,26 @@ export default {
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%
+  }
+
 </style>
